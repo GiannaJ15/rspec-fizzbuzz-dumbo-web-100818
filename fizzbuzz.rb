e1e66afe3d2810ed4972f3ab492c2e363ba11677
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-20 20:55:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 3 == 0)
-    puts "Fizz"
+    return "Fizz"
   elsif (int % 5 == 0)
-  puts "Buzz"
+  return "Buzz"
 elsif (int % 5==0 ) && (int % 3 ==0)
-puts "Fizz"
-  
+return "FizzBuzz"
+end 
+end
