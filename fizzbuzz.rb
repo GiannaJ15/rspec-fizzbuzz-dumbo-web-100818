eef144b5793b9a3d5df238df374e138b9bcbe625
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-20 20:59:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif (int % 5 == 0)
   return "Buzz"
-elsif (int % 5==0 ) && (int % 3 ==0)
+elsif [((int % 5==0 ) && (int % 3 ==0)) == true]
 return "FizzBuzz"
 end 
 end
-fizzbuzz(3)
